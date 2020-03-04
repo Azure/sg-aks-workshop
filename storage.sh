178c54ebc81db5d0e0703abeb4c8e9b058b8850d
ms-jasondel <jasondel@microsoft.com>
2020-03-04 13:47:58 -0500

    Adding key variable.

--- storage.sh
+++ storage.sh
@@ -5,10 +5,9 @@ STORAGE_ACCOUNT=${PREFIX}storage
 az storage account create -g $RG -n $STORAGE_ACCOUNT
 
 # get the key
-az storage account keys list -g $RG -n $STORAGE_ACCOUNT --query "[0].value"                                                                    ✔  10625  10:13:27
-
+STORAGE_KEY=$(az storage account keys list -g $RG -n $STORAGE_ACCOUNT --query "[0].value")                                                                   ✔  10625  10:13:27
 # create a secret
-kubectl create secret generic fruit-secret \                                                                                                         ✔  10628  10:14:13
-  --from-literal=azurestorageaccountname=$STORAGE_ACCOUNT \     
-  --from-literal=azurestorageaccountkey=$STORAGE_KEY \         
+kubectl create secret generic fruit-secret \
+  --from-literal=azurestorageaccountname=$STORAGE_ACCOUNT \
+  --from-literal=azurestorageaccountkey=$STORAGE_KEY \
   -n dev
